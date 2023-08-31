47c872ce022ae9c5b62a9d643263efdd55344a8e
SuggestAnime <143011008+SuggestAnime@users.noreply.github.com>
2023-09-01 02:19:57 +0530

    Add files via upload

---
+++ Contact/script.js
@@ -0,0 +1,22 @@
+const name =document.getElementById('name');
+const email =document.getElementById('email');
+const submit =document.getElementsByClassName('container')[0];
+
+submit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log("Clicked")
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "suggestmeanime@gmail.com",
+        Password : "7017F94C38290D05EB02D37D2CD52C53C4BE",
+        To : 'suggestmeanime@gmail.com',
+        From : 'suggestmeanime@gmail.com',
+        Subject : "New contact from suggest ani-me",
+        Body : "Name: " + document.getElementById("name").value
+                + "<br> Email: " + document.getElementById("email").value
+                + "<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("message sent")
+    );
+});
